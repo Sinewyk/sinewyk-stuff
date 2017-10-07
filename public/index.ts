25c7a8c0d7f7be2515c33299bee01b8074532f77
sinewyk <sinewyk@gmail.com>
2017-10-07 17:07:52 +0200

    meh, kiss

--- public/index.ts
+++ public/index.ts
@@ -1,18 +1,16 @@
 import { run } from '@cycle/run';
 import xs from 'xstream';
-import { div, makeDOMDriver, VNode } from '@cycle/dom';
+import { div, makeDOMDriver } from '@cycle/dom';
 import Site from '../components/Site';
 import Footer from '../components/Footer';
 
 import '../styles/body.css';
 
-const wrapInADiv = (x: Array<VNode>) => div(x);
-
 function main() {
   const site = Site();
   const footer = Footer();
   const sinks = {
-    DOM: xs.combine(site.DOM, footer.DOM).map(wrapInADiv),
+    DOM: xs.combine(site.DOM, footer.DOM).map(x => div(x)),
   };
   return sinks;
 }
