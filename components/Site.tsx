de6725de08711187e18b6ba678910bd3dfdeac4e
sinewyk <sinewyk@gmail.com>
2017-10-23 00:35:35 +0200

    kinda fake routing going on

--- components/Site.tsx
+++ components/Site.tsx
@@ -1,17 +1,21 @@
-import xs from 'xstream';
+import { Stream } from 'xstream';
+import { VNode } from '@cycle/dom';
 import { html } from 'snabbdom-jsx';
 
 import styles from './Site.css';
 
-function Site() {
+function Site(vdom$: Stream<VNode>) {
   return {
-    DOM: xs.of(
-      <div className={styles.root}>
-        <h1>Welcome to my stuff</h1>
-        <p>In the future there will be a blog and stuff</p>
-        <p>For now ... just chill</p>
-      </div>,
-    ),
+    DOM: vdom$.map(vdom => (
+      <div>
+        <div className={styles.root}>
+          <h1>Welcome to my stuff</h1>
+          <p>In the future there will be a blog and stuff</p>
+          <p>For now ... just chill</p>
+        </div>
+        {vdom}
+      </div>
+    )),
   };
 }
 
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,41 @@
+import { Location } from 'history';
 import xs, { Stream } from 'xstream';
 import { div, VNode } from '@cycle/dom';
+import { extractSinks } from 'cyclejs-utils';
 import Site from '../components/Site';
 import Footer from '../components/Footer';
 import { Sources, RootSinks } from './interfaces';
 
 import '../styles/body.css';
 
-function view(sources: Sources): Stream<VNode> {
-  const site = Site();
+function view(sources: Sources, vdom$: Stream<VNode>): Stream<VNode> {
+  const site = Site(vdom$);
   const footer = Footer(sources.Config);
 
   return xs.combine(site.DOM, footer.DOM).map(x => div(x));
 }
 
 export default function app(sources: Sources): RootSinks {
+  const history$ = sources.History;
+
+  const pageSinks$ = history$.map((location: Location) => {
+    const { pathname } = location;
+    console.log(pathname);
+
+    // fake routing, it's the homepage
+    return {
+      DOM: xs.of(
+        div(
+          'Just playing around with my router, posts and random things will be here soon',
+        ),
+      ),
+    };
+  });
+
+  const pageSinks = extractSinks(pageSinks$, ['DOM']);
+
   const sinks = {
-    DOM: view(sources),
+    DOM: view(sources, pageSinks.DOM),
     History: xs.never(),
   };
   return sinks;
