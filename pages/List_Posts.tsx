a8322c4a65a175e1ef7eb450501a80a41de66e49
sinewyk <sinewyk@gmail.com>
2017-11-14 23:26:56 +0100

    edit some styles

--- pages/List_Posts.tsx
+++ pages/List_Posts.tsx
@@ -1,12 +1,15 @@
 import { PageSources } from '../src/interfaces';
 import { createElement } from 'snabbdom-pragma';
 import styles from '../styles/reusable.css';
+import typo from '../styles/typography.css';
 
 export default function List_Posts(sources: PageSources) {
   return {
     DOM: sources.Params.map(() => (
       <div className={styles.container}>
-        <p>My routing is up \o/, no troll, posts are coming >.></p>
+        <p className={typo.reading}>
+          My routing is up \o/, no troll, posts are coming >.>
+        </p>
       </div>
     )),
   };
