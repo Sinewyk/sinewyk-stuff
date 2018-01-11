7814db2973cc2dba2ec531e756b489a66e39c502
sinewyk <sinewyk@gmail.com>
2018-01-11 23:56:32 +0100

    any

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,7 +3,7 @@ import { createElement } from 'snabbdom-pragma';
 
 import styles from './Container.css';
 
-function Container(_: {}, children: VNode) {
+function Container(_: any, children: VNode) {
   return <div className={styles.container}>{children}</div>;
 }
 
